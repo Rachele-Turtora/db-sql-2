e61b060df72f1c8d7f47347546f6853ec7b08036
Rachele-Turtora <racheleturtora21@gmail.com>
2024-07-02 23:03:28 +0200

    [UPD] group_by

--- group_by.sql
+++ group_by.sql
@@ -11,6 +11,31 @@ GROUP BY
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
+SELECT
+    COUNT(`id`) AS `num_teachers`,
+    `office_address`
+FROM
+    `teachers`
+GROUP BY
+    `office_address`;
+
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
+SELECT
+    AVG(`vote`) AS `avg_vote`,
+    `exam_id`
+FROM
+    `exam_student`
+GROUP BY
+    `exam_id`;
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT
+    COUNT(`id`) AS `degrees`,
+    `department_id`
+FROM
+    `degrees`
+GROUP BY
+    `department_id`;
